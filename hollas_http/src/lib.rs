81ed4f30d77d558bf975c77564724ae5aff1c7d4
Mikehollas123 <hollas@gmail.com>
2023-11-18 06:27:48 -0600

    Hooked up route and is working!! now has a builder pattern still single threaded

--- hollas_http/src/lib.rs
+++ hollas_http/src/lib.rs
@@ -1,26 +1,44 @@
 use std::{
-    fs,
     io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    collections::{HashMap}
+    net::{TcpListener, TcpStream}
 };
-use std::fmt::format;
+use regex::Regex;
 
+pub struct HttpServerBuilder<'a> {
+    host : &'a str,
+    port : i32,
+    routes: Vec<&'a str>,
+    functions: Vec<fn() ->&'a str>,
+}
+
+impl<'a> HttpServerBuilder<'a> {
+    pub  fn new(addr : & str, port : i32) -> HttpServerBuilder {
+
+        HttpServerBuilder{host: addr, port, routes: vec![], functions: vec![] }
+    }
+
+    pub fn add_route(&mut self, route : &'a str, function: fn() ->&'a str) -> &mut HttpServerBuilder<'a> {
+        self.routes.push(route);
+        self.functions.push(function);
+        self
+    }
+
+    pub fn build(&mut self) -> HttpServer{
+        HttpServer::new(self.host,self.port,&self.routes,&self.functions )
+    }
+}
 
 pub struct HttpServer<'a> {
     host : &'a str,
     port : i32,
-    routes :  HashMap<&'a str, fn()->&'a str>,
+    routes: &'a Vec<&'a str>,
+    functions: &'a Vec<fn() ->&'a str>,
 }
 
 impl<'a> HttpServer<'a> {
-    pub fn new(addr : & str, port : i32) -> HttpServer {
+    fn new(addr : &'a str, port : i32, routes : &'a Vec<&'a str> , functions: &'a Vec<fn() ->&'a str>) -> HttpServer<'a> {
 
-    HttpServer{host: addr, port, routes:  HashMap::new() }
-    }
-
-    pub fn add_route(&mut self, route : &'a str, function: fn() ->&'a str)  {
-        self.routes.insert(route,function);
+    HttpServer{host: addr, port, routes: &routes, functions: &functions }
     }
 
     pub fn run(self) -> (){
@@ -42,19 +60,25 @@ fn handle_connection(&self,mut stream: TcpStream) {
 
     //println!("Request: {:#?}", http_request);
 
+    for (index,route) in self.routes.iter().enumerate()
+    {
+        let re = Regex::new(route).unwrap();
 
-    let thing = match   &self.routes.get(&http_request[0][..]) {
-        Some(thing ) => thing,
-        None => panic!(),
-    };
-
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let length = contents.len();
-
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-
-    stream.write_all(response.as_bytes()).unwrap();
+        if re.is_match(&http_request[0])
+        {
+            self.send_ok_content(self.functions[index](),stream);
+            break;
+        }
+    }
 }
+    fn send_ok_content(&self,contents : &str,mut stream: TcpStream) {
+
+        let status_line = "HTTP/1.1 200 OK";
+        let length = contents.len();
+
+        let response =
+            format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use hollas_http:: {HttpServerBuilder};
 
-use hollas_http:: {HttpServer};
 fn main() {
 
-    let mut server = HttpServer::new( "127.0.0.1",7879);
-        server.add_route("GET / HTTP/1.1", doThing);
-
-    server.run();
+    let _server = HttpServerBuilder::new( "127.0.0.1",7879)
+        .add_route("GET / HTTP/1.1", do_thing)
+        .add_route("GET /again HTTP/1.1", do_thing2)
+        .build().run();
 }
 
-fn doThing<'a>() -> &'a str{
-    println!("Did it!");
-
+fn do_thing<'a>() -> &'a str{
     "Did it!"
-
+}
+fn do_thing2<'a>() -> &'a str{
+    "Did it Again!"
 }
